0ba5e0976870405154c74d7d17e4874a51322a27
Puyan Wei <puyanwei@gmail.com>
2018-06-30 12:53:21 +0100

    Corrected test syntax errors

--- test/morse-code-test.js
+++ test/morse-code-test.js
@@ -1,14 +1,30 @@
 describe('#run', () => {
     it('translates morse-code to English', () => {
-        expect run(true, "- .... .   .-- .. --.. .- .-. -..   --.- ..- .. -.-. -.- .-.. -.--   .--- .. -. -..- . -..   - .... .   --. -. --- -- . ...   -... . ..-. --- .-. .   - .... . -.--   ...- .- .--. --- .-. .. --.. . -.. .-.-.-").toEqual("the wizard quickly jinxed the gnomes before they vaporized.");
+        expect(
+            run(
+                true,
+                '- .... .   .-- .. --.. .- .-. -..   --.- ..- .. -.-. -.- .-.. -.--   .--- .. -. -..- . -..   - .... .   --. -. --- -- . ...   -... . ..-. --- .-. .   - .... . -.--   ...- .- .--. --- .-. .. --.. . -.. .-.-.-'
+            )
+        ).toEqual(
+            'the wizard quickly jinxed the gnomes before they vaporized.'
+        );
     });
     it('translates English to morse-code', () => {
-        expect run(false,"The wizard quickly jinxed the gnomes before they vaporized.").toEqual("- .... .   .-- .. --.. .- .-. -..   --.- ..- .. -.-. -.- .-.. -.--   .--- .. -. -..- . -..   - .... .   --. -. --- -- . ...   -... . ..-. --- .-. .   - .... . -.--   ...- .- .--. --- .-. .. --.. . -.. .-.-.-");
+        expect(
+            run(
+                false,
+                'The wizard quickly jinxed the gnomes before they vaporized.'
+            )
+        ).toEqual(
+            '- .... .   .-- .. --.. .- .-. -..   --.- ..- .. -.-. -.- .-.. -.--   .--- .. -. -..- . -..   - .... .   --. -. --- -- . ...   -... . ..-. --- .-. .   - .... . -.--   ...- .- .--. --- .-. .. --.. . -.. .-.-.-'
+        );
     });
     it('returns error string if invalid input', () => {
-        expect run(true, "..- .... ..-. .-.. --- .-    ...-").toEqual("Invalid Morse Code Or Spacing")
+        expect(run(true, '..- .... ..-. .-.. --- .-    ...-')).toEqual(
+            'Invalid Morse Code Or Spacing'
+        );
     });
     it('returns error string if invalid input', () => {
-        expect run(true, "").toEqual("Invalid Morse Code Or Spacing")
+        expect(run(true, '')).toEqual('Invalid Morse Code Or Spacing');
     });
 });
